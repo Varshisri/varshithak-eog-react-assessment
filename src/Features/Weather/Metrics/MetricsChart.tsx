0b1eb6e492d42a1511fb99aef8980038dde26aaa
Varshisri <43078017+Varshisri@users.noreply.github.com>
2020-12-08 18:33:17 -0500

    small fixes

--- src/Features/Weather/Metrics/MetricsChart.tsx
+++ src/Features/Weather/Metrics/MetricsChart.tsx
@@ -54,10 +54,10 @@ export const MetricsChart = (props: displayProps) => {
   if (fetching) return <LinearProgress />;
   return (
     <LineChart width={600} height={300} data={measurements} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-      <Line type="monotone" dataKey="metricName" stroke="#8884d8" />
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
       <XAxis dataKey="at" />
-      <YAxis dataKey="value" />
+      <YAxis />
       <Tooltip />
     </LineChart>
   );
